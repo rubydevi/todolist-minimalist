e4677fd5be8380226357fb082e4500df559869b6
rubydevi <ariesabyss@gmail.com>
2023-06-13 21:10:28 +0530

    Add Checkbox, icons and style similar to the template provided

--- src/index.js
+++ src/index.js
@@ -13,7 +13,22 @@ const renderTodoList = () => {
 
   tasks.forEach((task) => {
     const listItem = document.createElement('li');
-    listItem.textContent = task.description;
+    const listCheck = document.createElement('input');
+    listCheck.setAttribute('type', 'checkbox');
+    listCheck.checked = task.completed;
+
+    const label = document.createElement('label');
+    label.appendChild(listCheck);
+    label.append(task.description);
+
+    const dragRow = document.createElement('span');
+    dragRow.innerHTML = 
+    `
+    <i class="fa-solid fa-ellipsis-vertical"></i>
+    `;
+
+    listItem.appendChild(label);
+    listItem.appendChild(dragRow);
     listItem.classList.add(task.completed ? 'completed' : 'incomplete');
     todoList.appendChild(listItem);
   });
