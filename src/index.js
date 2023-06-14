071db0dff40a9424f051575b034ea5512378cb1e
rubydevi <ariesabyss@gmail.com>
2023-06-14 19:40:38 +0530

    Import required modules

--- src/index.js
+++ src/index.js
@@ -1,36 +1,4 @@
 import './style.css';
-
-const tasks = [
-  { description: 'Go to the gym', completed: false, index: 1 },
-  { description: 'Download youtube videos', completed: true, index: 2 },
-  { description: 'Call Monica back', completed: false, index: 3 },
-];
-
-const renderTodoList = () => {
-  const todoList = document.getElementById('todo-list');
-
-  tasks.sort((a, b) => a.index - b.index);
-
-  tasks.forEach((task) => {
-    const listItem = document.createElement('li');
-    const listCheck = document.createElement('input');
-    listCheck.setAttribute('type', 'checkbox');
-    listCheck.checked = task.completed;
-
-    const label = document.createElement('label');
-    label.appendChild(listCheck);
-    label.append(task.description);
-
-    const dragRow = document.createElement('span');
-    dragRow.innerHTML = `
-    <i class="fa-solid fa-ellipsis-vertical"></i>
-    `;
-
-    listItem.appendChild(label);
-    listItem.appendChild(dragRow);
-    listItem.classList.add(task.completed ? 'completed' : 'incomplete');
-    todoList.appendChild(listItem);
-  });
-};
-
-window.addEventListener('DOMContentLoaded', renderTodoList);
+import {
+  renderTodoList, addTask, editTaskDescription, loadTasksFromLocalStorage, saveTasksToLocalStorage,
+} from './modules/todoList.js';
