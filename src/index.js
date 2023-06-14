82acb535075ff65c22ef9bc4b6eebe023a070c67
rubydevi <ariesabyss@gmail.com>
2023-06-14 19:41:16 +0530

    Add an eventlistener for adding task

--- src/index.js
+++ src/index.js
@@ -2,3 +2,20 @@ import './style.css';
 import {
   renderTodoList, addTask, editTaskDescription, loadTasksFromLocalStorage, saveTasksToLocalStorage,
 } from './modules/todoList.js';
+
+window.addEventListener('DOMContentLoaded', loadTasksFromLocalStorage);
+window.addEventListener('beforeunload', saveTasksToLocalStorage);
+
+const todoInput = document.querySelector('.list-input input');
+const todoList = document.getElementById('todoList');
+
+// add tasks EventListener
+todoInput.addEventListener('keydown', (event) => {
+  if (event.keyCode === 13) {
+    const description = todoInput.value.trim();
+    if (description !== '') {
+      addTask(description);
+      todoInput.value = ''; // Clear the input field
+    }
+  }
+});
