4befe4565593c2dc552fc40f048a633e4e8d194b
rubydevi <ariesabyss@gmail.com>
2023-06-15 14:27:48 +0530

    Update statusUpdate.js according to new change

--- src/modules/statusUpdates.js
+++ src/modules/statusUpdates.js
@@ -1,6 +1,10 @@
-import { renderTodoList, saveTasksToLocalStorage, tasks } from './todoList.js';
+import {
+  renderTodoList, saveTasksToLocalStorage, getTasks, setTasks,
+} from './todoList.js';
 
 export const updateStatus = (index, completed) => {
+  const tasks = getTasks(); // Get tasks using the getter
+
   const updatedTasks = tasks.map((task) => {
     if (task.index === index) {
       return { ...task, completed };
@@ -8,13 +12,15 @@ export const updateStatus = (index, completed) => {
     return task;
   });
 
-  // Update tasks array and save to local storage
-  tasks.splice(0, tasks.length, ...updatedTasks);
+  // Update tasks array and save to local storage using the setter
+  setTasks(updatedTasks);
   saveTasksToLocalStorage();
   renderTodoList();
 };
 
 export const clearCompleted = () => {
+  const tasks = getTasks(); // Get tasks using the getter
+
   const filteredTasks = tasks.filter((task) => !task.completed);
 
   const updatedTasks = filteredTasks.map((task, index) => ({
@@ -22,8 +28,8 @@ export const clearCompleted = () => {
     index: index + 1,
   }));
 
-  // Update tasks array and save to local storage
-  tasks.splice(0, tasks.length, ...updatedTasks);
+  // Update tasks array and save to local storage using the setter
+  setTasks(updatedTasks);
   saveTasksToLocalStorage();
   renderTodoList();
 };
